5d07a2a66bb2b7db9212059867f3c9717c73b9d5
superman98888 <55739255+superman98888@users.noreply.github.com>
2019-10-03 11:30:23 +0700

    Add files via upload

---
+++ Excercise04-master/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github");
+        }
+        //input: float x
+        //output: smallest integral value that is not less than x
+        //idea: a = (int)x then a = a + 1
+        //if x is negative then a is unchanged
+        //if x == a then a is unchanged
+        public static int Ceil(float x)
+        {
+            int a = (int)x;
+            if (x > 0)
+            {
+                if (x > a)
+                {
+                    a = a + 1;
+                }
+            }
+            return a;
+        }
+        //input: float x
+        //output: int a that is largest but not bigger than x
+        //idea: a = (int)x
+        //if x is negative then a = a - 1
+        //if x == a then a is unchanged
+        public static int Floor(float x)
+        {
+            int a = (int)x;
+            if (x < 0 && x != a)
+                    a = a - 1;
+            return a;
+        }
+        public static int Rand(int x)
+        {
+            Random Rand = new Random();
+            int a = x = Rand.Next(0, 1);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Excercise04-master/ConsoleApplication1/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCeil()
+        {
+            int result1 = Program.Ceil((float)2.3);
+            Assert.AreEqual(3, result1);
+
+            int result2 = Program.Ceil(-2.3f);
+            Assert.AreEqual(-2, result2);
+
+            int result3 = Program.Ceil(3);
+            Assert.AreEqual(3, result3);
+        }
+         [TestMethod]
+        public void TestFloor()
+        {
+            int result1 = Program.Floor((float)2.3);
+            Assert.AreEqual(2, result1);
+
+            int result2 = Program.Floor((float)-3.4);
+            Assert.AreEqual(-4, result2);
+
+            int result3 = Program.Floor(5);
+            Assert.AreEqual(5, result3);
+        }
+    }
+}
